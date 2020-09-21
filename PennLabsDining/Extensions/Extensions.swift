140a98ae8b9d77b4450050351973cb03cba00f72
Daniel Duan <daniel4duan@gmail.com>
2020-09-21 15:31:35 -0700

    initial commit

---
+++ PennLabsDining/Extensions/Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions.swift
+//  PennLabsDining
+//
+//  Created by Daniel Duan on 9/21/20.
+//
+// Extension to the UIImageView to help with asynchronous loading
+
+import Foundation
+import UIKit
+
+extension UIImageView {
+    func load(url: String) {
+    
+    }
+}
