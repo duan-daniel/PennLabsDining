adc54e636765534afa6a7bdae050d43d93c39231
Daniel Duan <daniel4duan@gmail.com>
2020-09-21 00:07:14 -0700

    Initial Commit

---
+++ PennLabsDining/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PennLabsDining
+//
+//  Created by Daniel Duan on 9/21/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
